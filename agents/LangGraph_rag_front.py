20ac29eb3acbe6c115e7fe7cf99756be1d65194d
ZhengjunHUO <firelouiszj@hotmail.com>
2025-04-18 10:31:53 +0200

    test langgraph: improve frontend

--- agents/LangGraph_rag_front.py
+++ agents/LangGraph_rag_front.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import requests
 
-question = st.text_input("Question", "What security issue could we face while using the LLM ?")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if st.button("Submit"):
-    rslt = requests.post("http://localhost:8888/answer", json = {"question": question})
-    st.write("", rslt.json()["resp"])
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+prompt = st.chat_input("Ask something here")
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    rslt = requests.post("http://localhost:8888/answer", json = {"question": prompt})
+    with st.chat_message("ai"):
+        st.markdown(rslt.json()["resp"])
+    st.session_state.messages.append({"role": "ai", "content": rslt.json()["resp"]})
